07e83eef7de7c717dc0fffe6b732a98fc526f540
redhadron <redhadron@gmail.com>
2026-03-13 01:14:03 -0400

    refactor generate.py asset data constants

--- dev/generate.py
+++ dev/generate.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageChops
 """
 todo:
 replace scandir with listdir
+replace readlines with read
 """
 
 
@@ -16,24 +17,52 @@ HYTALE_ASSETS_PATH = "E:\Hytale Assets 20260221" # path to a folder in which you
 SEP = os.sep
 
 HYTALE_BLOCKTEXTURES_PATH = HYTALE_ASSETS_PATH + SEP + "Common" + SEP + "BlockTextures"
-MATERIALS = {
-  "Wood": {"LIST": list("Blackwood Darkwood Deadwood Drywood Goldenwood Greenwood Hardwood Lightwood Redwood Softwood Tropicalwood".split(" ")), "SUFFIX": "_Planks"},
-  "Rock": {"LIST": list("Basalt Quartzite Shale Stone Volcanic".split(" ")), "SUFFIX": "_Brick_Smooth"},
-}
+DATA_PAGES = [
+  [
+    ("TEXTURE_NAME_PREFIX", "Wood_"),
+    ("FAMILY_LIST", list("Blackwood Darkwood Deadwood Drywood Goldenwood Greenwood Hardwood Lightwood Redwood Softwood Tropicalwood".split(" "))),
+    ("TEXTURE_NAME_SUFFIX", "_Planks"),
+    ("JSON_TAGS_TYPE_STR", "Wood"),
+    ("JSON_TAGS_SUBTYPE", ",\n    \"SubType\": [\n      \"Planks\"\n    ]"),
+    ("JSON_TAGS_FAMILY", ",\n    \"Family\": [\n      \"${FAMILY}\"\n    ]"),
+    ("JSON_GATHERING_BREAKING_GATHERTYPE_STR", "Woods"),
+    ("JSON_FUEL_QUALITY_LINE", "\"FuelQuality\": 3.0,"),
+  ],
+  [
+    ("TEXTURE_NAME_PREFIX", "Rock_"),
+    ("FAMILY_LIST", list("Basalt Quartzite Shale Stone Volcanic".split(" "))),
+    ("TEXTURE_NAME_SUFFIX", "_Brick_Smooth"),
+    ("JSON_TAGS_TYPE_STR", "Rock"),
+    ("JSON_TAGS_SUBTYPE", ""),
+    ("JSON_TAGS_FAMILY", ",\n    \"Family\": [\n      \"${FAMILY}\"\n    ]"),
+    ("JSON_GATHERING_BREAKING_GATHERTYPE_STR", "Rocks"),
+    ("JSON_FUEL_QUALITY_LINE", ""),
+  ],
+]
 #  Aqua Calcite Gold Ledge Lime Marble # these are available as smooth bricks in-game but their textures have irregular names.
 # "Rock": {"LIST": ["Runic_Blue", "Runic_Dark", "Runic_Teal"], "SUFFIX": ""}, # irregular texture names
 SOIL_CLAY_COLORS = "Black Blue Cyan Green Grey Lime Orange Pink Purple Red White Yellow"
 # ocean is also like a color of soil clay (non-smooth naming), but I did not include it because it has no smooth counterpart.
 # there is also a regular clay: Soil_Clay.
 # there is also clay brick and ocean clay brick.
+def data_page_get_value(data_page, key):
+  assert isinstance(key, str) and isinstance(data_page, list)
+  for item in data_page:
+    assert len(item) == 2 and isinstance(item[0], str)
+    if item[0] == key:
+      return item[1]
+  raise KeyError(key)
+def data_page_has_key(data_page, key):
+  assert isinstance(key, str) and isinstance(data_page, list)
+  return any(item[0] == key for item in data_page)
 
-modPath = SEP.join([os.getcwd(), ".."])
-modelFolderPath = SEP.join([modPath, "Common", "Blocks", "Breeze"])
-assert os.path.exists(modelFolderPath)
-outputFolderPath = SEP.join([modPath, "Server", "Item", "Items"])
-assert os.path.exists(outputFolderPath)
-iconFolderPath = SEP.join([modPath, "Common", "Icons", "ItemsGenerated"])
-assert os.path.exists(iconFolderPath)
+MOD_PATH = SEP.join([os.getcwd(), ".."])
+MODEL_FOLDER_PATH = SEP.join([MOD_PATH, "Common", "Blocks", "Breeze"])
+assert os.path.exists(MODEL_FOLDER_PATH)
+OUTPUT_FOLDER_PATH = SEP.join([MOD_PATH, "Server", "Item", "Items"])
+assert os.path.exists(OUTPUT_FOLDER_PATH)
+ICON_FOLDER_PATH = SEP.join([MOD_PATH, "Common", "Icons", "ItemsGenerated"])
+assert os.path.exists(ICON_FOLDER_PATH)
 TEMPLATE_FILE_PATH = os.getcwd() + SEP + "Breeze_Template.json"
 assert os.path.exists(TEMPLATE_FILE_PATH)
 
@@ -59,39 +88,38 @@ if len(templateFileLines) == 0:
   raise ValueError("empty template file?? failed.")
   
   
-modelFileNamesList = list(dirEntry.name for dirEntry in os.scandir(modelFolderPath) if dirEntry.name.endswith(".blockymodel"))
+modelFileNamesList = list(dirEntry.name for dirEntry in os.scandir(MODEL_FOLDER_PATH) if dirEntry.name.endswith(".blockymodel"))
   
 for modelFileName in modelFileNamesList:
-  shapeName = remove_suffix(modelFileName, ".blockymodel")
-  shapeNameWithoutDepth = remove_suffix(shapeName, "_Db1000")
+  shapeNameWithDepth = remove_suffix(modelFileName, ".blockymodel")
+  shapeNameWithoutDepth = remove_suffix(shapeNameWithDepth, "_Db1000")
   iconMaskFileName = shapeNameWithoutDepth + ".png"
   
-  for materialType, materialData in MATERIALS.items():
-    for material in materialData["LIST"]:
-        
-      textureFileName = patch_texture_name(f"{materialType}_{material}{materialData['SUFFIX']}.png")
+  for dataPage in DATA_PAGES:
+    for family in data_page_get_value(dataPage, "FAMILY_LIST"):
+      unpatchedTextureFileBaseName = f"{data_page_get_value(dataPage, 'TEXTURE_NAME_PREFIX')}{family}{data_page_get_value(dataPage, 'TEXTURE_NAME_SUFFIX')}"
+      textureFileName = patch_texture_name(unpatchedTextureFileBaseName+".png")
       
-      assetInfo = {"full_name": materialType + "_" + material + "_" + shapeName}
-      assetInfo["output_file_path"] = outputFolderPath + SEP + assetInfo["full_name"] + ".json"
+      assetInfo = {"full_name": data_page_get_value(dataPage, "JSON_TAGS_TYPE_STR") + "_" + family + "_" + shapeNameWithoutDepth}
+      assetInfo["output_file_path"] = OUTPUT_FOLDER_PATH + SEP + assetInfo["full_name"] + ".json"
       assetInfo["icon_file_name"] = assetInfo["full_name"] + ".png"
-      assetInfo["icon_file_path"] = iconFolderPath + SEP + assetInfo["icon_file_name"]
+      assetInfo["icon_file_path"] = ICON_FOLDER_PATH + SEP + assetInfo["icon_file_name"]
       
       assetContents = {
         "ICON_PATH_IN_MOD": "Icons/ItemsGenerated/" + assetInfo["icon_file_name"],
-        "BLOCK_SET": f"{materialType}_{material}{materialData['SUFFIX']}",
+        "BLOCK_SET": unpatchedTextureFileBaseName,
         "TEXTURE_PATH_IN_MOD": f"BlockTextures/{textureFileName}",
-        "RESOURCE_TYPE_ID": f"{materialType}_{material}",
+        "RESOURCE_TYPE_ID_TO_CRAFT": f"{data_page_get_value(dataPage, 'JSON_TAGS_TYPE_STR')}_{family}",
       }
       
-      with Image.open(modelFolderPath + SEP + iconMaskFileName) as thumbnailMaskImage:
-        print(thumbnailMaskImage.getbbox())
+      with Image.open(MODEL_FOLDER_PATH + SEP + iconMaskFileName) as thumbnailMaskImage:
+        print(thumbnailMaskImage.size)
         with Image.open(HYTALE_BLOCKTEXTURES_PATH + SEP + textureFileName) as thumbnailTextureImage:
-          print(thumbnailTextureImage.getbbox())
+          print(thumbnailTextureImage.size)
           thumbnailResultImage = ImageChops.multiply(thumbnailMaskImage.convert("RGB"), thumbnailTextureImage.convert("RGB"))
           thumbnailResultImage.save(assetInfo["icon_file_path"])
-      
-      
-      
+          
+          
       outputFilePath = assetInfo["output_file_path"]
       if os.path.exists(outputFilePath):
         print(f"replacing {outputFilePath}")
@@ -103,11 +131,18 @@ for modelFileName in modelFileNamesList:
 
         for currentLine in templateFileLines:
           outputLine = currentLine.replace("${FULL_NAME}", assetInfo["full_name"]
-            ).replace("${MODEL_NAME}", shapeName
+            ).replace("${MODEL_BASE_NAME}", shapeNameWithDepth
             ).replace("${ICON_PATH_IN_MOD}", assetContents["ICON_PATH_IN_MOD"]
             ).replace("${SET}", assetContents["BLOCK_SET"]
-            ).replace("${RESOURCE_TYPE_ID}", assetContents["RESOURCE_TYPE_ID"]
-            ).replace("${TEXTURE_PATH_IN_MOD}", assetContents["TEXTURE_PATH_IN_MOD"])
-          assert "${" not in outputLine
+            ).replace("${RESOURCE_TYPE_ID_TO_CRAFT}", assetContents["RESOURCE_TYPE_ID_TO_CRAFT"]
+            ).replace("${TEXTURE_PATH_IN_MOD}", assetContents["TEXTURE_PATH_IN_MOD"]
+            ).replace("${JSON_TAGS_TYPE_STR}", data_page_get_value(dataPage, "JSON_TAGS_TYPE_STR")
+            ).replace("${JSON_TAGS_SUBTYPE}", data_page_get_value(dataPage, "JSON_TAGS_SUBTYPE")
+            ).replace("${JSON_TAGS_FAMILY}", data_page_get_value(dataPage, "JSON_TAGS_FAMILY")
+            ).replace("${JSON_GATHERING_BREAKING_GATHERTYPE_STR}", data_page_get_value(dataPage, "JSON_GATHERING_BREAKING_GATHERTYPE_STR")
+            ).replace("${JSON_FUEL_QUALITY_LINE}", data_page_get_value(dataPage, "JSON_FUEL_QUALITY_LINE")
+            ).replace("${FAMILY}", family # must happen after json_ items
+            )
+          assert "${" not in outputLine, outputLine
           outputFile.write(outputLine)
       
